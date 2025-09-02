f1e91d8a62721fe906afaa5047ab93c85f972d8c
Brayan6603 <brayanarroyo778@gmail.com>
2025-09-02 13:39:44 -0600

    Terminamos sumarserie

--- CalculosTDD/src/Calculos.java
+++ CalculosTDD/src/Calculos.java
@@ -10,7 +10,11 @@ public class Calculos {
      * @return sumatoria
      */
     int sumarSerie(int x, int y) {
-        throw new UnsupportedOperationException("En construcción.");
+        int suma = 0;
+	for(int i = x; i<y; i++){
+	suma=suma + i;
+}
+return suma;
     }
 
     /**
